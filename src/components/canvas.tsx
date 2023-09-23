f1ccacb728aa4f1ce8cb58db0affaec22c15e90c
halo <halo@rlxyz.com>
2023-09-23 15:37:32 +0800

    now canvas is being initialized

---
+++ src/components/canvas.tsx
@@ -0,0 +1,9 @@
+export const App = () => {
+  return (
+    <div className='bg-background min-h-screen'>
+      <div className='p-md h-screen flex items-center justify-center border border-accents_7 rounded-tertiary'>
+        <canvas width={400} height={400} className='border border-accents_7' />
+      </div>
+    </div>
+  )
+}
